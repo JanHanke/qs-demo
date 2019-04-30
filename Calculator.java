8d4fea15f1e3a27fb443f5014f931e01a58f606b
Jan Hanke <jan.hanke@diva-e.com>
2019-04-30 10:27:57 +0200

    Revert "Extend calculator to be always correct"
    This reverts commit 6a4c3e7652c3e54ef5da957be4ad8be22e21ef27.

--- Calculator.java
+++ Calculator.java
@@ -1,5 +1,10 @@
 public class Calculator {
-		public int calculateEverything(int a, int b) {
-				return 42;
+		public int add(int a, int b) {
+				return a + b;
+		}
+
+		public int mul(int a, int b) {
+				System.out.println("a * b = " + (a*b)); 
+				return a * b;
 		}
 }
